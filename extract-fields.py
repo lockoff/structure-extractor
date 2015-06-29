0f734c2485b75ec9a3a0a470d385e49ce94983ef
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-06-29 10:45:19 -0700

    Open one file at a time.

--- extract-fields.py
+++ extract-fields.py
@@ -7,9 +7,9 @@ def extract(data_dir):
     _, _, file_names = os.walk(data_dir).next()
     file_paths = [os.path.join(data_dir, filename) for filename in file_names if filename.endswith('.pdf')]
     print "Loading pdfquery for each pdf file."
-    pdfs = [pdfquery.PDFQuery(pdf_path) for pdf_path in file_paths]
     selector = 'LTTextBoxHorizontal:contains("%s") LTTextLineHorizontal:last-child'
-    for (pdf, path) in zip(pdfs, file_paths):
+    for path in file_paths:
+        pdf = pdfquery.PDFQuery(path)
         print "Loading XML Tree for pdf %s" % path
         pdf.load()
         json_path = path + '.json'
@@ -42,6 +42,7 @@ def extract(data_dir):
         print "Writing fields for pdf %s to json file %s" % (path, json_path)
         with open(json_path, 'w') as fp:
             json.dump(fields, fp, indent=4)
+        pdf.file.close()
 
 if __name__ == '__main__':
     extract(sys.argv[1])
